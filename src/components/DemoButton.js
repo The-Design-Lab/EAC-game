4e48b7c9366634b8e18d1915c42ef35c850030d8
Tyler Morales <moratyle@gmail.com>
2021-04-14 14:59:50 -0600

    💄 Create demo styled-components button

---
+++ src/components/DemoButton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// Styled component named StyledButton
+const StyledButton = styled.button`
+  padding: 20px 10px;
+`
+
+function Button() {
+  return <StyledButton>Button</StyledButton>
+}
+
+export default Button
