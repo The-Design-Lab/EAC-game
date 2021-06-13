02e21c4821befaa5c8b18c0a06123069943360a3
Tyler Morales <moratyle@gmail.com>
2021-06-13 15:56:59 -0500

    ♻️ Refactor choice css into an external js file

--- src/pages/Choice-2/Choice-2.js
+++ src/pages/Choice-2/Choice-2.js
@@ -1,64 +1,11 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
+import { MainContent, Buttons, Choices } from '../../styles/choicesSyles'
 import { Link } from 'react-router-dom'
 import Button from '../../components/Button'
 import '../../utilities.css'
 import usePlayer from '../../hooks/usePlayer'
 import useCheckInvestments from '../../hooks/useCheckInvestments'
 import INVESTMENTS_VEHICLES from '../../data/Investments'
-
-
-const MainContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-
-  h1 {
-    text-align: center;
-    font-family: var(--body);
-    font-weight: 400;
-  }
-  h2 {
-    font-size: 36px;
-    max-width: 800px;
-    margin: 0 auto;
-  }
-
-  p {
-    max-width: 800px;
-    margin: 0 auto;
-    text-align: center;
-  }
-
-  ul {
-    max-width: 800px;
-    margin: 0 auto;
-    list-style: center;
-
-    li {
-      margin-bottom: 25px;
-      list-style-type: decimal;
-
-      p {
-        text-align: left;
-      }
-    }
-  }
-`
-const Buttons = styled.div`
-  display: flex;
-  justify-content: center;
-`
-const Choices = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 20px;
-  gap: 50px;
-
-  button {
-    font-weight: 700;
-  }
-`
 
 const choiceData = {
   year: 2005,
