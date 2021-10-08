3c513c9dd843e26f3f2e1e34aaa63702fb81ffee
Rafael Santana <rsantana@colum.edu>
2021-10-08 14:14:06 -0500

    Added all definitions

---
+++ src/data/definitions.js
@@ -0,0 +1,207 @@
+export const dictionaryData = [
+  {
+    term: "401K",
+    definition:
+      "It is a retirement plan from your employer that lets you automatically save part of your salary before taxes.",
+  },
+  {
+    term: "APR",
+    definition: "Annual Percentage Rate - Average yearly rate of interest paid",
+  },
+  {
+    term: "Bond",
+    definition:
+      "An investment in which an investor loans money to a company or the government for a set time period and a set return (or interest rate) and the company or government is obligated to pay it back.",
+  },
+  {
+    term: "CD",
+    definition:
+      "A deposit or savings account that is held for a set period of time. (CDs often have a higher interest rate than savings accounts but you pay a penalty for early withdrawal.)",
+  },
+  {
+    term: "Compound Interest",
+    definition:
+      "The interest you earn interest on any interest you earned from your original investment.",
+  },
+  {
+    term: "Consumer Price Index (CPI)",
+    definition:
+      "A measure that examines the weighted average of prices of a basket of consumer goods and services. Inflation indicator.",
+  },
+  {
+    term: "Current account balance",
+    definition:
+      "Broadest measure of US transactions with the rest of the world including goods, services, and investments, payments overseas",
+  },
+  {
+    term: "Diversify",
+    definition: "To obtain a mix of investments to reduce risk",
+  },
+  {
+    term: "Emergency Savings",
+    definition:
+      "A savings fund for emergencies. Ideally, this fund should have enough enough money to cover your basic expenses for at least three months. With this in place, you are prepared in the event you lose a job or come face-to-face with unexpected costs from home or car repairs or medical emergencies",
+  },
+  {
+    term: "Employer Benefits",
+    definition:
+      "Employee benefits are defined as a form of compensation paid by employers to employees over and above regular salary or wages. Employee benefits come in many forms and are an important part of the overall compensation package offered to employees.",
+  },
+  {
+    term: "Employer Match",
+    definition:
+      "Money your employer gives you as a contribution or deposit in your retirement account as a percentage of the money that you set aside in your retirement account.",
+  },
+  {
+    term: "Equity investment",
+    definition: "Money that is invested by buying shares of a company. ",
+  },
+  {
+    term: "ETF",
+    definition:
+      "An exchange traded fund (ETF) is a basket of securities (similar to a mutual fund) that trade on an exchange, just like a stock.",
+  },
+  {
+    term: "FDIC",
+    definition:
+      "Federal Depository Insurance Corporation - Insurance the federal government provides to protect you against the loss of your deposits in a bank account (up to $250,000)",
+  },
+  {
+    term: "Fed Target Rate",
+    definition:
+      "Federal funds rate is the target interest rate set by the Federal Open Market Commision at which commercial banks borrow and lend their excess reserves to each other overnight.",
+  },
+  {
+    term: "Gross Domestic Product (GDP)",
+    definition:
+      "Total dollar value of all goods and services produced for a specific time period.",
+  },
+  {
+    term: "Index Fund",
+    definition:
+      "A group of stocks meeting selected criteria (ie. Standard and Poors 500 or S&P 500)",
+  },
+  {
+    term: "Industrial Production",
+    definition: "Measures changes in output for industrial sector",
+  },
+  {
+    term: "Inflation",
+    definition: "The increase in prices over time.",
+  },
+  {
+    term: "Interest",
+    definition:
+      "A small amount of money that you receive from a bank or financial institution in return for keeping your money in a  bank account or other savings tool.",
+  },
+  {
+    term: "IRA",
+    definition:
+      "A retirement account that allows you to invest with PRE-TAX dollars",
+  },
+  {
+    term: "Leading Indicators Index (if possible)",
+    definition:
+      "Index predicting the direction of global economic movements in future months (used to plan performance of economy)",
+  },
+  {
+    term: "Mutual Fund",
+    definition:
+      "An investment that pools individuals' funds together to buy a group or collection of stocks and bonds.",
+  },
+  {
+    term: "Nonfarm Payrolls",
+    definition:
+      "Statistical survey providing the number of Total US Workers excluding farm workers and a handful of other job classifications",
+  },
+  {
+    term: "PE Ratio",
+    definition:
+      "The price-earnings ratio (P/E ratio) relates a company's share price to its earnings per share. A high P/E ratio could mean that a company's stock is over-valued, or else that investors are expecting high growth rates in the future.",
+  },
+  {
+    term: "PMI",
+    definition:
+      "Private mortgage insurance, also called PMI, is a type of mortgage insurance you might be required to pay for if you have a conventional loan. ... PMI is usually required when you have a conventional loan and make a down payment of less than 20 percent of the home's purchase price",
+  },
+  {
+    term: "Potential Return",
+    definition: "The money you MAY make on your investment.",
+  },
+  {
+    term: "Principal",
+    definition: "The original money invested.",
+  },
+  {
+    term: "Producer Price Index (PPI)",
+    definition: "Measures the change in price of factory produced goods.",
+  },
+  {
+    term: "Purchasing Manager's Index",
+    definition:
+      "Index based on a survey of corporate purchasing managers on the state of the industry as they see.",
+  },
+  {
+    term: "Recession",
+    definition:
+      "A recession is a period of declining economic performance across an entire economy that lasts for several months.",
+  },
+  {
+    term: "Retail Sales",
+    definition: "Total of durable and non-durable goods sales to consumers.",
+  },
+  {
+    term: "Risk",
+    definition:
+      "The degree of uncertainty and/or potential financial loss associated with an investment decision.",
+  },
+  {
+    term: "Roth IRA",
+    definition:
+      "A retirement account in which post-tax dollars are invested, but earnings are not taxed",
+  },
+  {
+    term: "S&P 500",
+    definition:
+      "The S&P 500 is a stock market index that is viewed as a measure of how well the stock market is performing overall. It includes around 500 of the largest U.S. companies.",
+  },
+  {
+    term: "Share",
+    definition:
+      "A share is the single smallest denomination of a company's stock",
+  },
+  {
+    term: "Shiller PE",
+    definition:
+      "A valuation measure for equities that uses real per-share earnings over 10 years.",
+  },
+  {
+    term: "Stock",
+    definition: "An investment that means part ownership in a company.",
+  },
+  {
+    term: "Ten Year Treasury Rate",
+    definition:
+      "Representing the yields paid by government as interest for borrowing money by selling bonds (used as proxy of investor confidence)",
+  },
+  {
+    term: "Trade Balance",
+    definition:
+      "Difference between US exports and imports of goods and services. Largest component of current account balance",
+  },
+  {
+    term: "Treasury",
+    definition:
+      "The U.S. Treasury is a government department in charge of managing government revenue and currency",
+  },
+  {
+    term: "Unemployment Claims",
+    definition:
+      "Number of individuals who filed for unemployment for the first time during the past week.",
+  },
+  {
+    term: "Unemployment Rate",
+    definition:
+      "The unemployment rate is the proportion of the labor force that is not currently employed but could be.",
+  },
+];
